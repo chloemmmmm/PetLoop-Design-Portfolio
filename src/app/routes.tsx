d70ab128a0fac492fa11c0bb95fa7393901eb6bb
chloemmmmm <1710756188@qq.com>
2026-09-01 01:33:12 +0800

    feat: route PetLoop interactive console

--- src/app/routes.tsx
+++ src/app/routes.tsx
@@ -1,11 +1,12 @@
 import { Route, Routes } from 'react-router-dom'
 import { CaseStudyPage } from '../pages/CaseStudyPage'
+import { DemoPage } from '../pages/DemoPage'
 
 export function AppRoutes() {
   return (
     <Routes>
       <Route path="/" element={<CaseStudyPage />} />
-      <Route path="/demo" element={<main><p>Simulated Data · Concept Demo</p></main>} />
+      <Route path="/demo" element={<DemoPage />} />
     </Routes>
   )
 }
